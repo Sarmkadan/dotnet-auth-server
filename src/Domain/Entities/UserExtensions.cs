d4b85ab2e45fae3ab49b11328446e09e773e62cc
Vladyslav Zaiets <rutova2@gmail.com>
2025-07-18 14:46:22 +0300

    Extension methods on User

--- src/Domain/Entities/UserExtensions.cs
+++ src/Domain/Entities/UserExtensions.cs
@@ -12,16 +12,19 @@ using System.Linq;
 namespace DotnetAuthServer.Domain.Entities;
 
 /// <summary>
-/// Extension methods for the User entity providing additional functionality
+/// Provides extension methods for the <see cref="User"/> entity to enhance functionality
+/// with additional domain-specific operations.
 /// </summary>
 public static class UserExtensions
 {
     /// <summary>
-    /// Checks if the user has a specific role
+    /// Checks if the user has a specific role.
     /// </summary>
-    /// <param name="user">The user instance</param>
-    /// <param name="role">The role to check for</param>
-    /// <returns>True if user has the role, false otherwise</returns>
+    /// <param name="user">The user instance.</param>
+    /// <param name="role">The role to check for.</param>
+    /// <returns>True if user has the role; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="user"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="role"/> is null or whitespace.</exception>
     public static bool HasRole(this User user, string role)
     {
         ArgumentNullException.ThrowIfNull(user);
@@ -31,30 +34,30 @@ public static class UserExtensions
     }
 
     /// <summary>
-    /// Checks if the user has any of the specified roles
+    /// Checks if the user has any of the specified roles.
     /// </summary>
-    /// <param name="user">The user instance</param>
-    /// <param name="roles">Collection of roles to check</param>
-    /// <returns>True if user has any of the roles, false otherwise</returns>
+    /// <param name="user">The user instance.</param>
+    /// <param name="roles">Collection of roles to check.</param>
+    /// <returns>True if user has any of the roles; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="user"/> or <paramref name="roles"/> is null.</exception>
     public static bool HasAnyRole(this User user, IEnumerable<string> roles)
     {
         ArgumentNullException.ThrowIfNull(user);
         ArgumentNullException.ThrowIfNull(roles);
 
-        if (!roles.Any())
-            return false;
-
         return roles.Any(role => user.HasRole(role));
     }
 
     /// <summary>
-    /// Gets an attribute value by key with optional default value
+    /// Gets an attribute value by key with optional default value.
     /// </summary>
-    /// <param name="user">The user instance</param>
-    /// <param name="key">The attribute key</param>
-    /// <param name="defaultValue">Default value if key not found</param>
-    /// <typeparam name="T">Type of the attribute value</typeparam>
-    /// <returns>The attribute value or default if not found</returns>
+    /// <typeparam name="T">Type of the attribute value.</typeparam>
+    /// <param name="user">The user instance.</param>
+    /// <param name="key">The attribute key.</param>
+    /// <param name="defaultValue">Default value if key not found.</param>
+    /// <returns>The attribute value or default if not found.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="user"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="key"/> is null or whitespace.</exception>
     public static T? GetAttribute<T>(this User user, string key, T? defaultValue = default)
     {
         ArgumentNullException.ThrowIfNull(user);
@@ -69,11 +72,13 @@ public static class UserExtensions
     }
 
     /// <summary>
-    /// Sets an attribute value
+    /// Sets an attribute value.
     /// </summary>
-    /// <param name="user">The user instance</param>
-    /// <param name="key">The attribute key</param>
-    /// <param name="value">The attribute value</param>
+    /// <param name="user">The user instance.</param>
+    /// <param name="key">The attribute key.</param>
+    /// <param name="value">The attribute value.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="user"/>, <paramref name="key"/>, or <paramref name="value"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="key"/> is null or whitespace.</exception>
     public static void SetAttribute(this User user, string key, object value)
     {
         ArgumentNullException.ThrowIfNull(user);
@@ -85,10 +90,11 @@ public static class UserExtensions
     }
 
     /// <summary>
-    /// Checks if the user is an administrator
+    /// Checks if the user is an administrator.
     /// </summary>
-    /// <param name="user">The user instance</param>
-    /// <returns>True if user has Admin role, false otherwise</returns>
+    /// <param name="user">The user instance.</param>
+    /// <returns>True if user has Admin role; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="user"/> is null.</exception>
     public static bool IsAdmin(this User user)
     {
         ArgumentNullException.ThrowIfNull(user);
@@ -97,10 +103,11 @@ public static class UserExtensions
     }
 
     /// <summary>
-    /// Gets the display name for the user (FullName if available, otherwise Username)
+    /// Gets the display name for the user (FullName if available, otherwise Username).
     /// </summary>
-    /// <param name="user">The user instance</param>
-    /// <returns>Display name string</returns>
+    /// <param name="user">The user instance.</param>
+    /// <returns>Display name string.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="user"/> is null.</exception>
     public static string GetDisplayName(this User user)
     {
         ArgumentNullException.ThrowIfNull(user);
@@ -109,10 +116,11 @@ public static class UserExtensions
     }
 
     /// <summary>
-    /// Checks if the user's email is verified and account is active
+    /// Checks if the user's email is verified, account is active, and not locked.
     /// </summary>
-    /// <param name="user">The user instance</param>
-    /// <returns>True if user can authenticate, false otherwise</returns>
+    /// <param name="user">The user instance.</param>
+    /// <returns>True if user can authenticate; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="user"/> is null.</exception>
     public static bool CanAuthenticate(this User user)
     {
         ArgumentNullException.ThrowIfNull(user);
@@ -121,10 +129,11 @@ public static class UserExtensions
     }
 
     /// <summary>
-    /// Gets the time elapsed since last login in seconds
+    /// Gets the time elapsed since last login in seconds.
     /// </summary>
-    /// <param name="user">The user instance</param>
-    /// <returns>Seconds since last login, or null if never logged in</returns>
+    /// <param name="user">The user instance.</param>
+    /// <returns>Seconds since last login, or null if never logged in.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="user"/> is null.</exception>
     public static long? SecondsSinceLastLogin(this User user)
     {
         ArgumentNullException.ThrowIfNull(user);
