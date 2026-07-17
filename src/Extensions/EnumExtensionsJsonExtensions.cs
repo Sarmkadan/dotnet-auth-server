eaa124d4c8737e8b4c4582db18c1c1615f55de9e
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 14:20:14 +0300

    minor

--- src/Extensions/EnumExtensionsJsonExtensions.cs
+++ src/Extensions/EnumExtensionsJsonExtensions.cs
@@ -15,81 +15,78 @@ using System.Text.Json.Serialization;
 /// </summary>
 public static class EnumExtensionsJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = false,
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-    };
+	private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false,
+		DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+	};
 
-    /// <summary>
-    /// Serializes the enum value to a JSON string representation.
-    /// </summary>
-    /// <typeparam name="TEnum">The enum type.</typeparam>
-    /// <param name="value">The enum value to serialize.</param>
-    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-    /// <returns>A JSON string representation of the enum value.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson<TEnum>(this TEnum value, bool indented = false)
-        where TEnum : struct, Enum
-    {
-        ArgumentNullException.ThrowIfNull(value);
+	/// <summary>
+	/// Serializes the enum value to a JSON string representation using camelCase naming.
+	/// </summary>
+	/// <typeparam name="TEnum">The enum type to deserialize.</typeparam>
+	/// <param name="value">The enum value to serialize.</param>
+	/// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+	/// <returns>A JSON string representation of the enum value.</returns>
+	public static string ToJson<TEnum>(this TEnum value, bool indented = false)
+	where TEnum : struct, Enum
+	{
+		var options = indented
+			? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+			: _jsonOptions;
 
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
-            : _jsonOptions;
+		return JsonSerializer.Serialize(value, options);
+	}
 
-        return JsonSerializer.Serialize(value, options);
-    }
+	/// <summary>
+	/// Deserializes a JSON string to an enum value.
+	/// </summary>
+	/// <typeparam name="TEnum">The enum type to deserialize.</typeparam>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>The deserialized enum value, or null if the JSON is invalid.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is empty or whitespace.</exception>
+	public static TEnum? FromJson<TEnum>(string json)
+	where TEnum : struct, Enum
+	{
+		ArgumentNullException.ThrowIfNull(json);
+		ArgumentException.ThrowIfNullOrWhiteSpace(json);
 
-    /// <summary>
-    /// Deserializes a JSON string to an enum value.
-    /// </summary>
-    /// <typeparam name="TEnum">The enum type to deserialize.</typeparam>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized enum value, or null if the JSON is invalid.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is empty or whitespace.</exception>
-    public static TEnum? FromJson<TEnum>(string json)
-        where TEnum : struct, Enum
-    {
-        ArgumentNullException.ThrowIfNull(json);
-        ArgumentException.ThrowIfNullOrWhiteSpace(json);
+		try
+		{
+			return JsonSerializer.Deserialize<TEnum>(json, _jsonOptions);
+		}
+		catch (JsonException)
+		{
+			return null;
+		}
+	}
 
-        try
-        {
-            return JsonSerializer.Deserialize<TEnum>(json, _jsonOptions);
-        }
-        catch (JsonException)
-        {
-            return null;
-        }
-    }
+	/// <summary>
+	/// Attempts to deserialize a JSON string to an enum value.
+	/// </summary>
+	/// <typeparam name="TEnum">The enum type to deserialize.</typeparam>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">Receives the deserialized enum value, or null if deserialization fails.</param>
+	/// <returns>True if deserialization succeeded; otherwise, false.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is empty or whitespace.</exception>
+	public static bool TryFromJson<TEnum>(string json, out TEnum? value)
+	where TEnum : struct, Enum
+	{
+		ArgumentNullException.ThrowIfNull(json);
+		ArgumentException.ThrowIfNullOrWhiteSpace(json);
 
-    /// <summary>
-    /// Attempts to deserialize a JSON string to an enum value.
-    /// </summary>
-    /// <typeparam name="TEnum">The enum type to deserialize.</typeparam>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized enum value, or null if deserialization fails.</param>
-    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is empty or whitespace.</exception>
-    public static bool TryFromJson<TEnum>(string json, out TEnum? value)
-        where TEnum : struct, Enum
-    {
-        ArgumentNullException.ThrowIfNull(json);
-        ArgumentException.ThrowIfNullOrWhiteSpace(json);
-
-        try
-        {
-            value = JsonSerializer.Deserialize<TEnum>(json, _jsonOptions);
-            return true;
-        }
-        catch (JsonException)
-        {
-            value = default;
-            return false;
-        }
-    }
+		try
+		{
+			value = JsonSerializer.Deserialize<TEnum>(json, _jsonOptions);
+			return true;
+		}
+		catch (JsonException)
+		{
+			value = default;
+			return false;
+		}
+	}
 }
