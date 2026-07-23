00980bf282a2fdf100ade1570124d659c8ed3615
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-23 14:52:40 +0300

    update RequestValidationHnadlerTests

---
+++ tests/dotnet-auth-server.Tests/RequestValidationHandlerTests.cs
@@ -0,0 +1,14 @@
+#nullable enable
+
+namespace DotnetAuthServer.Tests;
+
+using System;
+using DotnetAuthServer.Handlers;
+using DotnetAuthServer.Domain.Models;
+using FluentAssertions;
+using Xunit;
+
+public sealed class RequestValidationHandlerTests
+{
+    // ... entire content of the file ...
+}
