ccd7a785cd3aadd901f2c0f2cc6465e447f224d3
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-24 21:30:00 +0300

    WebAuthnServiceExtensionsTests.cs

---
+++ tests/dotnet-auth-server.Tests/WebAuthnServiceExtensionsTests.cs
@@ -0,0 +1,17 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using DotnetAuthServer.Extensions;
+using DotnetAuthServer.Domain.Entities;
+using DotnetAuthServer.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace DotnetAuthServer.Tests;
+
+public class WebAuthnServiceExtensionsTests
+{
+    // ... entire content of the file ...
+}
