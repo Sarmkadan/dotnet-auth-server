d81bb83649e044d219fef58bfa1e3dfed35d80a7
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-27 21:30:00 +0300

    Update ScopeValidationServiceTests
    should be backwards compatible.

---
+++ tests/dotnet-auth-server.Tests/ScopeValidationServiceTests.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+
+namespace dotnet_auth_server.Tests
+{
+    public class ScopeValidationServiceTests
+    {
+        [Fact]
+        public async Task Test_ValidateScopesAsync_ValidInput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Test_GetRequiredScopes_ValidInput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Test_ContainsRequiredScopes_ValidInput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Test_MergeScopes_ValidInput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Test_FilterScopes_ValidInput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Test_ValidateScopesAsync_InvalidInput_Null()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Test_ValidateScopesAsync_InvalidInput_EmptyCollection()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Test_ValidateScopesAsync_InvalidInput_BoundaryValue()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Test_ValidateScopesAsync_ErrorPath_ExpectedException()
+        {
+            // Test implementation
+        }
+    }
+}
